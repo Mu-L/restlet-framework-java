4c70afa2765de67894cd44b9471d93b534e3ea83
Jerome Louvel <374450+jlouvel@users.noreply.github.com>
2024-10-27 19:54:24 -0400

    Updated copyright headers
    Focusing on Apache 2,0 license,

--- org.restlet.gwt/org.restlet.gwt/src/main/java/org/restlet/client/resource/ExceptionHandler.java
+++ org.restlet.gwt/org.restlet.gwt/src/main/java/org/restlet/client/resource/ExceptionHandler.java
@@ -1,3 +1,12 @@
+/**
+ * Copyright 2005-2024 Qlik
+ * 
+ * The contents of this file is subject to the terms of the Apache 2.0 open
+ * source license available at http://www.opensource.org/licenses/apache-2.0
+ * 
+ * Restlet is a registered trademark of QlikTech International AB.
+ */
+
 package org.restlet.client.resource;
 
 public interface ExceptionHandler<E extends Throwable> {
--- org.restlet.java/org.restlet.test/src/main/java/org/restlet/test/TemplateFilterTestCase.java
+++ org.restlet.java/org.restlet.test/src/main/java/org/restlet/test/TemplateFilterTestCase.java
@@ -1,24 +1,9 @@
 /**
  * Copyright 2005-2024 Qlik
- * <p>
- * The contents of this file are subject to the terms of one of the following
- * open source licenses: Apache 2.0 or or EPL 1.0 (the "Licenses"). You can
- * select the license that you prefer but you may not use this file except in
- * compliance with one of these Licenses.
- * <p>
- * You can obtain a copy of the Apache 2.0 license at
- * http://www.opensource.org/licenses/apache-2.0
- * <p>
- * You can obtain a copy of the EPL 1.0 license at
- * http://www.opensource.org/licenses/eclipse-1.0
- * <p>
- * See the Licenses for the specific language governing permissions and
- * limitations under the Licenses.
- * <p>
- * Alternatively, you can obtain a royalty free commercial license with less
- * limitations, transferable or non-transferable, directly at
- * https://restlet.talend.com/
- * <p>
+ * 
+ * The contents of this file is subject to the terms of the Apache 2.0 open
+ * source license available at http://www.opensource.org/licenses/apache-2.0
+ * 
  * Restlet is a registered trademark of QlikTech International AB.
  */
 
